af3115377ac5c069e141a21ed28452b7377848f8
xuchen-imac <chenxu2656@gmail.com>
2022-06-06 18:47:11 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 const defaultIgnore = ['Dockerfile',".git",'.gitignore','.dockerignore','node_modules','package.json',"yarn.lock","package-lock.json"]
 import officegen from 'officegen'
+import readline from 'readline'
 let docx = officegen({
     type: 'docx',
     pageSize: "A4",
@@ -29,6 +30,9 @@ const exportCode = (pathList,filename)=>{
         const con = fs.readFileSync(file,'utf-8')
         allCon += '\n'
         allCon += con
+        fs.writeFileSync('test.txt',con,{flag: 'a'},(err)=>{
+            console.log(err);
+        })
     })
     const docsFile = fs.createWriteStream(`./${filename}.docx`)
     // 写入内容
@@ -44,8 +48,23 @@ const exportCode = (pathList,filename)=>{
     //     font_face: 'Times New Roman',
     // })
     docx.generate(docsFile)
+    const readstream = fs.createReadStream('/Users/xuchen/Desktop/myPro/exportCodetoWord/test.txt')
+    const lineRead = readline.createInterface({
+        input: readstream
+    })
+    let arr = []
+    lineRead.on('line',(data)=>{
+        arr.push(data)
+    }).on('close',()=>{
+        console.log(arr.length);
+        if(arr.length < 3480) {
+            console.log('xiaoyu');
+        }else {
+            
+        }
+    })
 }
-
+// 每个页面58行
 const ignoreFolder = ['Docs','images','assets']
 
 const res = exportFilePath('/Users/xuchen/Desktop/aitmed/aitmed/provider',ignoreFolder)
